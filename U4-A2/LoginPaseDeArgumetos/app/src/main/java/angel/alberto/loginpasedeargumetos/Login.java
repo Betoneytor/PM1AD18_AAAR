f85aca17f1b263d2d917c95aac26462d06bc7d7e
Alberto Angel Aguado Reyes <betoneitor333@gmail.com>
2018-09-04 23:31:52 -0500

    Añado el proyecto de el login con pase de datos, sin embargo no recuerdo como cumplir con la caracteriztica de enviar datos

---
+++ U4-A2/LoginPaseDeArgumetos/app/src/main/java/angel/alberto/loginpasedeargumetos/Login.java
@@ -0,0 +1,32 @@
+package angel.alberto.loginpasedeargumetos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends Activity {
+    EditText txtusuario, txtpass;
+    Button btnboton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txtusuario = findViewById(R.id.TxtUsuario);
+        txtpass = findViewById(R.id.TxtPassword);
+        btnboton =findViewById(R.id.btnLogin);
+
+        btnboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent respuesta = new Intent();
+                respuesta.putExtra("usuario",txtusuario.getText().toString());
+                respuesta.putExtra("pass",txtpass.getText().toString());
+                setResult(MainActivity.LOGIN_INTENT_CODE,respuesta);
+                finish();
+            }
+        });
+    }
+        }
