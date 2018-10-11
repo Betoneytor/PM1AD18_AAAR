8cecdcc8e39fe4748d07e11c2e49aac7ffcdd498
Alberto Angel Aguado Reyes <betoneitor333@gmail.com>
2018-10-11 00:23:55 -0500

    Entrega de practica de fragments basica

---
+++ U6-A1/UsoBasicoDeFragments/app/src/main/java/angel/alberto/usobasicodefragments/MainActivity.java
@@ -0,0 +1,49 @@
+package angel.alberto.usobasicodefragments;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnFragmentoA = (Button)  findViewById(R.id.btnFragmentoA);
+        btnFragmentoA.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               //getFragmentManager()
+               getSupportFragmentManager()
+                       .beginTransaction()
+                       .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                       .replace(R.id.contenedor, new FragmentA())
+                       .commit();
+
+           }
+
+
+        });
+
+        Button btnFragmentoB = (Button)  findViewById(R.id.btnFragmentoB);
+        btnFragmentoB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //getFragmentManager()
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.contenedor, new FragmentB())
+                        .commit();
+
+            }
+
+
+        });
+
+    }
+}
